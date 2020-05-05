1307e7250e2b175d56c05f81c85449aeed7405be
Magnus Kulke <mkulke@gmail.com>
2020-05-05 03:13:07 +0200

    Support polygons for ways

---
+++ src/osm/geo.rs
@@ -0,0 +1,136 @@
+use geo::prelude::*;
+use geo::COORD_PRECISION;
+use geo_types::{Geometry, LineString, Point, Polygon};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Location {
+    lat: f64,
+    lon: f64,
+}
+
+impl PartialEq<Location> for Location {
+    fn eq(&self, other: &Self) -> bool {
+        let self_point = Point::new(self.lon, self.lat);
+        let other_point = Point::new(other.lon, other.lat);
+        let distance = self_point.haversine_distance(&other_point);
+        distance < COORD_PRECISION.into()
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Bounds {
+    e: f64,
+    n: f64,
+    s: f64,
+    w: f64,
+}
+
+impl From<&Bounds> for (Location, Location) {
+    fn from(bounds: &Bounds) -> Self {
+        let ne = Location {
+            lon: bounds.n,
+            lat: bounds.e,
+        };
+        let sw = Location {
+            lon: bounds.s,
+            lat: bounds.w,
+        };
+
+        (ne, sw)
+    }
+}
+
+impl PartialEq<Bounds> for Bounds {
+    fn eq(&self, other: &Self) -> bool {
+        let (self_ne, self_sw) = self.into();
+        let (other_ne, other_sw) = other.into();
+        self_ne == other_ne && self_sw == other_sw
+    }
+}
+
+fn get_geometry(coordinates: Vec<(f64, f64)>) -> Option<Geometry<f64>> {
+    let line_string: LineString<f64> = coordinates.into();
+    let first = line_string.points_iter().next()?;
+    let last = line_string.points_iter().last()?;
+    if first == last {
+        let polygon = Polygon::new(line_string, vec![]);
+        Some(Geometry::Polygon(polygon))
+    } else {
+        Some(Geometry::LineString(line_string))
+    }
+}
+
+fn get_bounds(geometry: &Geometry<f64>) -> Option<Bounds> {
+    let rect = match geometry {
+        Geometry::LineString(ls) => ls.bounding_rect(),
+        Geometry::Polygon(p) => p.bounding_rect(),
+        _ => None,
+    }?;
+    Some(Bounds {
+        e: rect.max().x,
+        n: rect.max().y,
+        s: rect.min().y,
+        w: rect.min().x,
+    })
+}
+
+fn get_centroid(geometry: &Geometry<f64>) -> Option<Location> {
+    let point = match geometry {
+        Geometry::LineString(ls) => ls.centroid(),
+        Geometry::Polygon(p) => p.centroid(),
+        _ => None,
+    }?;
+    Some(Location {
+        lat: point.lat(),
+        lon: point.lng(),
+    })
+}
+
+pub fn get_geo_info(coordinates: Vec<(f64, f64)>) -> (Option<Location>, Option<Bounds>) {
+    if let Some(geo) = get_geometry(coordinates) {
+        let centroid = get_centroid(&geo);
+        let bounds = get_bounds(&geo);
+        (centroid, bounds)
+    } else {
+        (None, None)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_geo_info_open() {
+        let coordinates = vec![(5., 49.), (6., 50.), (7., 49.)];
+        let (centroid, bounds) = get_geo_info(coordinates);
+        let reference_loc = Location { lat: 49.5, lon: 6. };
+        assert_eq!(centroid.unwrap(), reference_loc);
+        let reference_bounds = Bounds {
+            e: 7.,
+            n: 50.,
+            s: 49.,
+            w: 5.,
+        };
+        assert_eq!(bounds.unwrap(), reference_bounds);
+    }
+
+    #[test]
+    fn get_geo_info_closed() {
+        let coordinates = vec![(5., 49.), (6., 50.), (7., 49.), (5., 49.)];
+        let (centroid, bounds) = get_geo_info(coordinates);
+        let reference_loc = Location {
+            lat: 49.333_333,
+            lon: 6.,
+        };
+        assert_eq!(centroid.unwrap(), reference_loc);
+        let reference_bounds = Bounds {
+            e: 7.,
+            n: 50.,
+            s: 49.,
+            w: 5.,
+        };
+        assert_eq!(bounds.unwrap(), reference_bounds);
+    }
+}
--- src/osm/mod.rs
+++ src/osm/mod.rs
@@ -1,6 +1,5 @@
+use self::geo::{get_geo_info, Bounds, Location};
 use filter::{filter, Group};
-use geo::prelude::*;
-use geo_types::LineString;
 use osmpbfreader::objects::{NodeId, OsmId, OsmObj, Tags};
 use osmpbfreader::OsmPbfReader;
 use serde::{Deserialize, Serialize};
@@ -10,6 +9,7 @@ use std::error::Error;
 use std::io::{Read, Seek, Write};
 
 pub mod filter;
+mod geo;
 
 #[derive(Serialize, Deserialize)]
 struct JSONNode {
@@ -22,20 +22,6 @@ struct JSONNode {
 }
 
 #[derive(Serialize, Deserialize)]
-struct Location {
-    lat: f64,
-    lon: f64,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Bounds {
-    e: f64,
-    n: f64,
-    s: f64,
-    w: f64,
-}
-
-#[derive(Serialize, Deserialize)]
 struct JSONWay {
     id: i64,
     #[serde(rename = "type")]
@@ -45,36 +31,15 @@ struct JSONWay {
     bounds: Option<Bounds>,
 }
 
-fn get_line_string(objs: &BTreeMap<OsmId, OsmObj>, node_ids: &[NodeId]) -> LineString<f64> {
-    let coordinates: Vec<(f64, f64)> = node_ids
+fn get_coordinates(objs: &BTreeMap<OsmId, OsmObj>, node_ids: &[NodeId]) -> Vec<(f64, f64)> {
+    node_ids
         .iter()
         .filter_map(|id| {
             let obj = objs.get(&OsmId::Node(*id))?;
             obj.node()
         })
         .map(|node| (node.lon(), node.lat()))
-        .collect();
-    coordinates.into()
-}
-
-fn get_bounds(objs: &BTreeMap<OsmId, OsmObj>, node_ids: &[NodeId]) -> Option<Bounds> {
-    let line_string = get_line_string(objs, node_ids);
-    let rect = line_string.bounding_rect()?;
-    Some(Bounds {
-        e: rect.max().x,
-        n: rect.max().y,
-        s: rect.min().y,
-        w: rect.min().x,
-    })
-}
-
-fn get_centroid(objs: &BTreeMap<OsmId, OsmObj>, node_ids: &[NodeId]) -> Option<Location> {
-    let line_string = get_line_string(objs, node_ids);
-    let point = line_string.centroid()?;
-    Some(Location {
-        lat: point.lat(),
-        lon: point.lng(),
-    })
+        .collect()
 }
 
 pub fn process(
@@ -84,8 +49,9 @@ pub fn process(
 ) -> Result<(), Box<dyn Error>> {
     let mut pbf = OsmPbfReader::new(file);
     let objs = pbf.get_objs_and_deps(|obj| filter(obj, groups))?;
+
     for obj in objs.values() {
-        if !filter(obj, groups) {
+        if !filter(&obj, groups) {
             continue;
         }
 
@@ -102,8 +68,8 @@ pub fn process(
                 writeln!(writer, "{}", jn_str)?;
             }
             OsmObj::Way(way) => {
-                let centroid = get_centroid(&objs, &way.nodes);
-                let bounds = get_bounds(&objs, &way.nodes);
+                let coordinates = get_coordinates(&objs, &way.nodes);
+                let (centroid, bounds) = get_geo_info(coordinates);
                 let jw = JSONWay {
                     osm_type: "way",
                     id: way.id.0,
