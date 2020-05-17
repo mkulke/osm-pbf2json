85f804e23fe3ee16775cda1b41cdd6972a97171e
Magnus Kulke <mkulke@gmail.com>
2020-05-17 16:25:23 +0200

    Added integration test for nodes

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -4,19 +4,22 @@ use osm_pbf2json::{filter, process};
 use std::fs::File;
 use std::io::{Cursor, Read, Seek, SeekFrom};
 
+fn get_string(cursor: &mut Cursor<Vec<u8>>) -> String {
+    cursor.seek(SeekFrom::Start(0)).unwrap();
+    let mut out = Vec::new();
+    cursor.read_to_end(&mut out).unwrap();
+    String::from_utf8(out).unwrap()
+}
+
 #[test]
-fn find_amenities() {
+fn find_fountains_or_townhalls() {
     let mut cursor = Cursor::new(Vec::new());
     let groups = filter::parse("amenity~fountain+tourism,amenity~townhall".to_string());
     let file = File::open("./tests/data/alexanderplatz.pbf").unwrap();
     process(file, &mut cursor, &groups).unwrap();
 
-    cursor.seek(SeekFrom::Start(0)).unwrap();
-    let mut out = Vec::new();
-    cursor.read_to_end(&mut out).unwrap();
-    let string = String::from_utf8(out).unwrap();
+    let string = get_string(&mut cursor);
     let lines: Vec<&str> = string.trim().split('\n').collect();
-
     assert_eq!(lines.len(), 4);
     for line in lines {
         assert!(
@@ -24,4 +27,16 @@ fn find_amenities() {
                 || line.contains(r#"amenity":"townhall"#)
         );
     }
+}
+
+#[test]
+fn find_bike_parking_for_six() {
+    let mut cursor = Cursor::new(Vec::new());
+    let groups = filter::parse("amenity~bicycle_parking+capacity~6".to_string());
+    let file = File::open("./tests/data/alexanderplatz.pbf").unwrap();
+    process(file, &mut cursor, &groups).unwrap();
+
+    let string = get_string(&mut cursor);
+    let lines: Vec<&str> = string.trim().split('\n').collect();
+    assert_eq!(lines.len(), 14);
 }
