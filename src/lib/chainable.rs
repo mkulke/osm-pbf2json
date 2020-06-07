e62786b73c3aa718e3b6477249c300ba81746b55
Magnus Kulke <mkulke@gmail.com>
2020-06-07 15:50:42 +0200

    Moved chain fn out of trait

--- src/lib/chainable.rs
+++ src/lib/chainable.rs
@@ -1,5 +1,4 @@
 pub trait Chainable<T> {
-    fn chain(&self) -> Vec<Vec<T>>;
     fn merge(&mut self);
 }
 
@@ -37,49 +36,49 @@ impl<T: Copy> Prependable<T> for Vec<T> {
     }
 }
 
-impl<T: Copy + PartialEq> Chainable<T> for Vec<Vec<T>> {
-    fn chain(&self) -> Vec<Vec<T>> {
-        use Connection::*;
+fn chain<T: Copy + PartialEq>(chainable: &mut Vec<Vec<T>>) -> Vec<Vec<T>> {
+    use Connection::*;
 
-        let mut chains: Vec<Vec<T>> = vec![];
-        for list in self {
-            let first_elem = list.first();
-            let last_elem = list.last();
-            if let Some(connection) = chains.iter_mut().find_map(|chain| {
-                let list_first = first_elem?;
-                let list_last = last_elem?;
-                let chain_first = chain.first()?;
-                let chain_last = chain.last()?;
-                if *chain_last == *list_first {
-                    Some(Tail(chain))
-                } else if *chain_first == *list_last {
-                    Some(Head(chain))
-                } else if *chain_last == *list_last {
-                    Some(ReverseTail(chain))
-                } else if *chain_first == *list_first {
-                    Some(ReverseHead(chain))
-                } else {
-                    None
-                }
-            }) {
-                match connection {
-                    Tail(chain) => chain.extend(&list[1..]),
-                    Head(chain) => chain.prepend(&list[..list.len() - 1]),
-                    ReverseTail(chain) => chain.reverse_extend(&list[..list.len() - 1]),
-                    ReverseHead(chain) => chain.reverse_prepend(&list[1..]),
-                }
+    let mut chains: Vec<Vec<T>> = vec![];
+    for list in chainable {
+        let first_elem = list.first();
+        let last_elem = list.last();
+        if let Some(connection) = chains.iter_mut().find_map(|chain| {
+            let list_first = first_elem?;
+            let list_last = last_elem?;
+            let chain_first = chain.first()?;
+            let chain_last = chain.last()?;
+            if *chain_last == *list_first {
+                Some(Tail(chain))
+            } else if *chain_first == *list_last {
+                Some(Head(chain))
+            } else if *chain_last == *list_last {
+                Some(ReverseTail(chain))
+            } else if *chain_first == *list_first {
+                Some(ReverseHead(chain))
             } else {
-                chains.push(list.to_vec());
+                None
             }
+        }) {
+            match connection {
+                Tail(chain) => chain.extend(&list[1..]),
+                Head(chain) => chain.prepend(&list[..list.len() - 1]),
+                ReverseTail(chain) => chain.reverse_extend(&list[..list.len() - 1]),
+                ReverseHead(chain) => chain.reverse_prepend(&list[1..]),
+            }
+        } else {
+            chains.push(list.to_vec());
         }
-        chains
     }
+    chains
+}
 
+impl<T: Copy + PartialEq> Chainable<T> for Vec<Vec<T>> {
     fn merge(&mut self) {
         let mut vec_size;
         loop {
             vec_size = self.len();
-            *self = self.chain();
+            *self = chain(self);
             if self.len() == vec_size {
                 break;
             }
